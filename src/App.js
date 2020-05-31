7c575d3f50e4cd82fb0beb443afa09235541c606
Abuubakry-Ali <abuubakry.ali@.com>
2020-05-31 19:19:06 -0400

    2nd commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from "./header/header"
+import Slide from "./slide/slide"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Slide/>
+    </div>
+  );
+}
+
+export default App;
